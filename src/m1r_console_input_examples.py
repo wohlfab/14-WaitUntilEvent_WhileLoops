091480d3ffa532f0e0681deddf8944749dc7b817
wohlfab <wohlfab@rose-hulman.edu>
2019-01-09 13:51:26 -0500

    Prep 14

--- src/m1r_console_input_examples.py
+++ src/m1r_console_input_examples.py
@@ -7,36 +7,36 @@ This module demonstrates how to INPUT from the CONSOLE:
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
          and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.  Read and run this program.  Then do the following problems,
+# DONE: 2.  Read and run this program.  Then do the following problems,
 #   putting your answers RIGHT HERE IN THIS DOCUMENT.
 #
 #   1. Write a line of code that would input an INTEGER from the
 #      console, storing the integer in a variable called 'x'.
 #      Write your line here:
-#
+#       x = int(input('Enter an integer: '))
 #   2. Write a line of code that would input an FLOAT from the console,
 #      storing the float in a variable called 'x'.
 #      Write your line here:
-#
+#       x = float(input('Put a float:'))
 #   3. Write a line of code that would input an STRING from the console,
 #      storing the string in a variable called 'x'.
 #      Write your line here:
-#
+#       x = str(input('Put a string:')
 #   4. What happens if you (the user) enter something OTHER than a
 #      single integer (e.g., you enter
-#        five
+#        five gives an error
 #      or
-#        4.5
+#        4.5 gives an error
 #      or
-#        1 1 1
+#        1 1 1 gives an error
 #      or
 #        nothing at all (just press the Enter key)
 #      -- try them!) when running the   input_an_integer   example?
 #      Put your answer here:
-#
+#       gives an error
 #   After you have PUT YOUR ANSWERS IN THIS COMMENT as described above,
 #     a. Find someone who has had HER answer checked.
 #        Ask her to check YOUR answers to the above.
