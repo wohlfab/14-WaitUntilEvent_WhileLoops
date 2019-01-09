6bc840a59ece91581b85ef2d6835513f5e485355
wohlfab <wohlfab@rose-hulman.edu>
2019-01-09 14:04:40 -0500

    Prep 14

--- src/m2_console_input.py
+++ src/m2_console_input.py
@@ -3,9 +3,9 @@ This module demonstrates lets you practice INPUT from the CONSOLE.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
-
+         and Brandon Wohlfarth.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
+import math
 
 def main():
     """ TESTs the functions in this module (by calling them). """
@@ -31,10 +31,11 @@ def double_a_float():
          -6.28
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
-
+    x = float(input('Put a floating point number: '))
+    print(2*x)
 
 def print_an_integer_many_times():
     """
@@ -62,9 +63,12 @@ def print_an_integer_many_times():
          10
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+    x = int(input('Put a positive integer: '))
+    for k in range(x):
+        print(x*2)
 
 
 def print_an_integer_many_times_on_one_row():
@@ -81,13 +85,17 @@ def print_an_integer_many_times_on_one_row():
          1010101010
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #   The testing code is already written for you (above).
     #
     # HINT: One way to print on a SINGLE line is to build up a string
     #       and then print that (single) string.
     # -------------------------------------------------------------------------
-
+    x = int(input('Put a positive integer: '))
+    list = ''
+    for k in range(x):
+        list = list + str(2*x)
+    print(list)
 
 def input_it_all():
     """
@@ -121,9 +129,17 @@ def input_it_all():
          Peace & Love.
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+    x = float(input('Put a positive floating point number: '))
+    y = int(input('Put a positive integer: '))
+    z = str(input('Put a string: '))
+    for k in range(y):
+        print(math.sqrt(x))
+    for k in range(y):
+        print(z)
+
 
 
 # -----------------------------------------------------------------------------
